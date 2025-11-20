ec89de56de4d1202cbf892a869a257ac1c120775
kaihui.wang <hpuwang@gmail.com>
2025-11-20 16:06:36 +0800

    fix(layout): 修复皮肤条件判断的逻辑错误
    - 将皮肤判断条件从 'devinggo' 改为 'default'
    - 确保深色模式开关在默认皮肤下显示
    - 修正相关模板中皮肤变量的引用
    - 优化皮肤设置逻辑的可维护性

--- src/layout/setting.vue
+++ src/layout/setting.vue
@@ -47,7 +47,7 @@
           <a-option value="ma-slide-up">{{ $t('sys.animate.sliderUp') }}</a-option>
         </a-select>
       </a-form-item>
-      <a-form-item :label="$t('sys.dark')" :help="$t('sys.darkHelp')" v-if="currentSkin === 'devinggo'">
+      <a-form-item :label="$t('sys.dark')" :help="$t('sys.darkHelp')" v-if="appStore.skin === 'default'">
         <a-switch v-model="form.mode" @change="handleSettingMode" />
       </a-form-item>
       <a-form-item :label="$t('sys.tag')" :help="$t('sys.tagHelp')">
